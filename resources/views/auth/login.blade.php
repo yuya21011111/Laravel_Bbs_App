cc8e316164a3f9968469c7fc0f07b816de09ab7b
YuyaYamazi <kaimomo12345@gmail.com>
2023-08-26 16:44:34 +0900

    ログインフォームデザイン変更

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,19 +8,30 @@
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('メールアドレス')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <div>
+                <div class="relative">
+                  <input id="email"  type="email" name="email" :value="old('email')" required   class="py-3 px-4 pl-11 block w-full border-gray-200 shadow-sm rounded-md text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400" placeholder="test@test.com">
+                  <div class="absolute inset-y-0 left-0 flex items-center pointer-events-none z-20 pl-4">
+                    <svg class="h-4 w-4 text-gray-400" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                      <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
+                    </svg>
+                  </div>
+                </div>
+            </div>
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('パスワード')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
+            <div>
+                <div class="relative">
+                  <input id="password"  type="password" name="password" :value="old('password')" required autocomplete="current-password"  class="py-3 px-4 pl-11 block w-full border-gray-200 shadow-sm rounded-md text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400">
+                  <div class="absolute inset-y-0 left-0 flex items-center pointer-events-none z-20 pl-4">
+                    <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" width="200" height="121.441" viewBox="0 0 200 121.441"><path d="M60.737,0a60.718,60.718,0,1,0,59.447,72.947h79.807V28.51H183.844V54.721h-15.1V28.51H152.6V54.721H121.157A60.718,60.718,0,0,0,60.737,0ZM59.119,21.833A38.922,38.922,0,1,1,21.89,61.527,38.922,38.922,0,0,1,59.119,21.833Z" transform="translate(0.009)"/></svg>
+                  </div>
+                </div>
+            </div>
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
